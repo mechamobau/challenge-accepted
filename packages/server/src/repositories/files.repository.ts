0e6f48edfe964279ec597837703956844ca1d4d7
Lucas Viana <vianadossantoslucas@gmail.com>
2020-06-18 16:45:42 -0300

    :sparkles: creates file repository

---
+++ packages/server/src/repositories/files.repository.ts
@@ -0,0 +1,38 @@
+import { join, resolve } from 'path';
+import { readFile } from 'fs';
+
+type Location = {
+  id: number;
+  name: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+};
+
+interface IFileRepository {
+  get: () => Promise<Location[]>;
+}
+
+class FileRepository implements IFileRepository {
+  fileName: string;
+  path: string;
+
+  constructor(fileName: string) {
+    this.fileName = fileName;
+    this.path = resolve(__dirname, `../../data/${this.fileName}.json`);
+  }
+
+  get = () =>
+    new Promise<Location[]>((resolve, reject) => {
+      readFile(this.path, (error, data) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        resolve(JSON.parse(data.toString()) ?? []);
+      });
+    });
+}
+
+export default FileRepository;
